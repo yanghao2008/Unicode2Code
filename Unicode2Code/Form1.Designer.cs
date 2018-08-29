0621c802a6f25f6931ecab1b6871df0ae4891300
yanghao2008 <42794956+yanghao2008@users.noreply.github.com>
2018-08-29 14:05:27 +0800

    Initial commit

---
+++ Unicode2Code/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Unicode2Code
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(30, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(376, 130);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(30, 174);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(376, 137);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(431, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Unicode2Code";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(431, 181);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Code2Unicode";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(429, 249);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2、unicode代码用\\分隔";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(429, 227);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "1、回车参与转化";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 333);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Unicode2Code";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Unicode2Code/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+
+namespace Unicode2Code
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int code = 0;
+
+            string str1 =richTextBox1.Text;
+            string str2 = "";
+            StringInfo ss = new StringInfo(str1);
+            int m = ss.LengthInTextElements;//字符长度
+
+            string word = "";
+            for (int i = 0; i < m; i++)
+            {
+                word = ss.SubstringByTextElements(i, 1);
+                    code = Char.ConvertToUtf32(word, 0);
+                    str2 = str2+ "\\" + Convert.ToString(code,16);
+
+            }
+            richTextBox2.Text = str2;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int code = 0;
+
+            string str1 = richTextBox1.Text;
+            string str2 = "";
+            str1 = str1.Replace("\n", "");
+            str1 = str1.Replace("\u200b", "");
+            StringInfo ss = new StringInfo(str1);
+
+            int m = ss.LengthInTextElements;//字符长度
+
+            string word = "";
+            string strKey = "";
+            for (int i = 0; i < m; i++)
+            {
+                word = ss.SubstringByTextElements(i, 1);
+
+                if (word == "\\")
+                {
+                    if (strKey != "")
+                    {
+                        try
+                        {
+                            code = Convert.ToInt32(strKey, 16);
+                        }
+                        catch (FormatException)
+                        {
+                            MessageBox.Show("有非法字符！");
+                        }
+
+                        str2 = str2 + Char.ConvertFromUtf32(code);
+                    }
+                    strKey = "";
+                }
+                else
+                {
+                    strKey += word;
+                }
+            }
+            if (strKey != "")
+            {
+                try { 
+                code = Convert.ToInt32(strKey, 16);
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("有非法字符！");
+                }
+                str2 = str2 + Char.ConvertFromUtf32(code);
+
+            }
+
+            richTextBox2.Text = str2;
+
+        }
+    }
+}
